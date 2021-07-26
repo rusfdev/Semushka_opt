f6853b8465eceaf9d42a0eb78e60c9dd1ddc5483
Ruslan Belozertsev <38186770+ruslanmmr@users.noreply.github.com>
2021-07-26 22:10:59 +0300

    eddited

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -7,15 +7,38 @@ const brakepoints = {
 }
 
 const $wrapper = document.querySelector('.wrapper');
-const $header = document.querySelector('.header');
 
 document.addEventListener("DOMContentLoaded", function() {
   CustomInteractionEvents.init();
+  Header.init();
+  calculator();
+  change_package();
+
+  //Product sliders
+  document.querySelectorAll('.product-slider').forEach($this => {
+    new ProductSlider($this).init();
+  })
+  //Image sliders
+  document.querySelectorAll('.image-slider').forEach($this => {
+    new ImageSlider($this).init();
+  })
+  //toggle
+  document.querySelectorAll('[data-toggle="parent"]').forEach($this => {
+    new ToggleElement($this).init();
+  })
+  //select
+  document.querySelectorAll('.select').forEach($this => {
+    new SlimSelect({
+      select: $this,
+      showSearch: false,
+      showContent: 'down'
+    })
+  })
 });
 
 const CustomInteractionEvents = Object.create({
   targets: {
-    value: 'a, button, [data-custom-interaction]'
+    value: 'a, button, label, [data-custom-interaction], .ss-single-selected, .ss-option'
   },
   touchEndDelay: {
     value: 100
@@ -85,3 +108,152 @@ const CustomInteractionEvents = Object.create({
     document.addEventListener('contextmenu', this.events);
   }
 })
+
+
+function calculator() {
+
+  let events = (event) => {
+    let $target = event.target.closest('.count-calculator__button');
+
+    if($target) {
+      let $input = $target.parentNode.querySelector('input');
+
+      if($target.classList.contains('count-calculator__button_minus')) {
+        $input.value = Math.max(+$input.value - 1, 1)
+      } else {
+        $input.value = +$input.value + 1
+      }
+    }
+  }
+
+  let input = (event) => {
+    let $target = event.target.closest('.count-calculator__input');
+
+    if($target) {
+      $target.value = Math.max(+$target.value, 1)
+    }
+  }
+
+  document.addEventListener('click', events);
+  document.addEventListener('input', input);
+}
+
+function change_package() {
+  let events = (event) => {
+    let $target = event.target.closest('.product-card__package input');
+
+    if($target) {
+      let $value = $target.closest('.product-card').querySelector('.product-card__calculator-count span');
+
+      if($value) $value.innerHTML = $target.value;
+    }
+  }
+
+  document.addEventListener('change', events);
+}
+
+class ProductSlider {
+  constructor($parent) {
+    this.$parent = $parent
+  }
+
+  init() {
+    this.$slider = this.$parent.querySelector('.swiper-container');
+    this.$prev = this.$parent.querySelector('.swiper-button-prev');
+    this.$next = this.$parent.querySelector('.swiper-button-next');
+
+    this.swiper = new Swiper(this.$slider, {
+      touchStartPreventDefault: false,
+      slidesPerView: 1,
+      speed: 500,
+      navigation: {
+        prevEl: this.$prev,
+        nextEl: this.$next
+      },
+      breakpoints: {
+        [brakepoints.xl]: {
+          slidesPerView: 6
+        }
+      }
+    });
+
+
+  }
+}
+
+class ImageSlider {
+  constructor($parent) {
+    this.$parent = $parent
+  }
+
+  init() {
+    this.$slider = this.$parent.querySelector('.swiper-container');
+    this.$pagination = this.$parent.querySelector('.swiper-pagination');
+
+    this.swiper = new Swiper(this.$slider, {
+      touchStartPreventDefault: false,
+      slidesPerView: 1,
+      speed: 500,
+      lazy: {
+        loadOnTransitionStart: true,
+        loadPrevNext: true
+      },
+      pagination: {
+        el: this.$pagination,
+        clickable: true,
+        bulletElement: 'button'
+      }
+    });
+
+
+  }
+}
+
+class ToggleElement {
+  constructor($parent) {
+    this.$parent = $parent;
+  }
+
+  init() {
+    this.$trigger = this.$parent.querySelector('[data-toggle="trigger"]');
+    this.$block = this.$parent.querySelector('[data-toggle="content"]');
+
+    this.$trigger.addEventListener('click', () => {
+      let state = this.$parent.classList.contains('is-active');
+
+      if(!state) this.open();
+      else this.close();
+    })
+    
+  }
+
+  open() {
+    for(let $el of [this.$parent, this.$trigger, this.$block]) {
+      $el.classList.add('is-active')
+    }
+  }
+
+  close() {
+    for(let $el of [this.$parent, this.$trigger, this.$block]) {
+      $el.classList.remove('is-active')
+    }
+  }
+}
+
+const Header = {
+  $element: document.querySelector('.header'),
+
+  init: function () {
+
+    let check = () => {
+      let y = $wrapper.getBoundingClientRect().y,
+          fixed = this.$element.classList.contains('header_fixed');
+
+      if (y <= 0 && !fixed) this.$element.classList.add('header_fixed');
+      else if (y > 0 && fixed) this.$element.classList.remove('header_fixed');
+    }
+
+    window.addEventListener('scroll', check)
+    check();
+  }
+}
